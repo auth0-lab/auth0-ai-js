82e822c3f1cffdd12064f8450cc0e87456fe431c
Jose F. Romaniello <jose@okta.com>
2025-02-27 15:19:20 -0300

    fix fed connection demo

--- demos/vercel-ai/lib/auth0-ai.ts
+++ demos/vercel-ai/lib/auth0-ai.ts
@@ -5,11 +5,11 @@ import { auth0 } from "./auth0";
 const auth0AI = new Auth0AI();
 
 export const withCalendarFreeBusyAccess = auth0AI.withFederatedConnection({
-  getRefreshToken: async () => {
+  refreshToken: async () => {
     const session = await auth0.getSession();
     const refreshToken = session?.tokenSet.refreshToken! as string;
     return refreshToken;
   },
-  connection: 'google-oauth2',
+  connection: "google-oauth2",
   scopes: ["https://www.googleapis.com/auth/calendar.freebusy"],
 });
