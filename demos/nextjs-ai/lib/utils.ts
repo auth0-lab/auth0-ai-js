34a431bb3816086955ce73e05b057080b09a3ba7
jcenturion-okta <javier.centurion@okta.com>
2025-03-10 15:38:22 -0300

    chore: adding user-menu on nextjs demo

--- demos/nextjs-ai/lib/utils.ts
+++ demos/nextjs-ai/lib/utils.ts
@@ -1,3 +1,10 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function generateUUID(): string {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0;
