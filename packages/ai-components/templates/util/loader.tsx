a20394704b2714bede50db16c94de5df0c7945b8
Deepu K Sasidharan <d4udts@gmail.com>
2025-07-21 19:48:14 +0200

    fix(ai-cmoponents): lint and FederatedConnections/popup.tsx useCallback refs (#248)
    * fix: request is required for latest sdk
    
    * fix: lint errors

--- packages/ai-components/templates/util/loader.tsx
+++ packages/ai-components/templates/util/loader.tsx
@@ -2,7 +2,7 @@ export const WaitingMessage = () => {
   return (
     <div className="flex flex-col gap-2 w-full">
       <div className="flex flex-col gap-4 text-muted-foreground">
-        Waiting...
+        Waiting for Authorization...
       </div>
     </div>
   );
